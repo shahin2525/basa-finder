fab5c213d5a0d1a96f42489f4104d8fd622a5013
shahin2525 <shahin451999@gmail.com>
2025-04-06 22:27:05 +0530

    add providers

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -3,7 +3,7 @@
 import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
 import { useState, useEffect } from "react";
-import { Button } from "./ui/button";
+import { Button } from "../ui/button";
 import {
   DropdownMenu,
   DropdownMenuContent,
