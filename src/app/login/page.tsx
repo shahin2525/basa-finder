fcc844ce0ab8e74451ad8db04d7ec5995ad3a179
shahin2525 <shahin451999@gmail.com>
2025-04-07 14:40:07 +0530

    add register

---
+++ src/app/login/page.tsx
@@ -0,0 +1,11 @@
+import LoginForm from "@/components/modules/auth/login/LoginForm";
+
+const LoginPage = () => {
+  return (
+    <div>
+      <LoginForm />
+    </div>
+  );
+};
+
+export default LoginPage;
